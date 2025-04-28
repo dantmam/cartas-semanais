d6991491848337bc4e5225697eb887460d699cf4
dantmam <danielssj85@gmail.com>
2025-04-28 03:28:00 -0300

    carta 7

--- style.js
+++ style.js
@@ -4,7 +4,7 @@ if (form) {
     form.addEventListener("submit", function(event) {
         event.preventDefault(); // Evita o envio normal do formulário
 
-        let respostaCorreta = "Quadro do naruto";
+        let respostaCorreta = "Conjunto de tweed";
         let radios = document.getElementsByName("alternativa");
         let respostaSelecionada = "";
         let loading = document.getElementById("loading");
@@ -32,7 +32,7 @@ if (form) {
                 mensagem.classList.add("verde");
 
                 setTimeout(() => {
-                    window.location.href = "carta-semanal-6.html";
+                    window.location.href = "carta-semanal-7.html";
                 }, 2000);
             } else {
                 mensagem.textContent = "Acho que você não gosta de mim, tente novamente.";
